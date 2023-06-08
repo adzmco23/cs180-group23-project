6b805ff3e53da23964b1316ae3f72e6072b0c170
adzmco23 <112993167+adzmco23@users.noreply.github.com>
2023-06-08 19:46:00 +0800

    added sample req in app.py; removed numpy from requirements

--- app/app.py
+++ app/app.py
@@ -14,8 +14,9 @@ def hello():
 # route for calculating food price using regression model
 @app.get("/price")
 def foodPrice():
-    param = request.args.get('p')
-    print(param)
+    # param = request.args.get('p')
+    # sample client request params:
+    param = {'year':2023, 'month':5, 'latitude':14.604167, 'longitude':120.982222, 'inflation':6.6, 'rice (special)':1, 'Retail':1}
     return str(m.getPrice(param))
 
 if __name__ == "__main__":
