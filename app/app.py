bc792fc9c3873dd1a8eeefd55489fa3f892acb71
adzmco23 <112993167+adzmco23@users.noreply.github.com>
2023-06-11 15:38:14 +0800

    updated app

--- app/app.py
+++ app/app.py
@@ -8,15 +8,26 @@ m: Model = Model()
 
 # render webpage
 @app.get("/")
-def hello():
+def home():
     return render_template('index.html')
 
 # route for calculating food price using regression model
 @app.get("/price")
 def foodPrice():
-    # param = request.args.get('p')
-    # sample client request params:
-    param = {'year':2023, 'month':5, 'latitude':14.604167, 'longitude':120.982222, 'inflation':6.6, 'rice (special)':1, 'Retail':1}
+
+    ### SAMPLE GET request: http://localhost:5000/price?year=2023&month=5&commodity=eggs&pricetype=wholesale
+
+    req = request.args.to_dict()
+    year = int(req['year'])
+    month = int(req['month'])
+    commodity = req['commodity']
+    pricetype = req['pricetype']
+
+    print(req)
+    param = {'year':year, 'month': month, 'latitude':14.604167, 'longitude':120.982222, 'inflation':6.6}
+    param.update({commodity: 1})
+    param.update({pricetype:1})
+    print(param)
     return str(m.getPrice(param))
 
 if __name__ == "__main__":
--- app/regression.py
+++ app/regression.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import MinMaxScaler
 
 class Model:
     def __init__(self):
@@ -35,19 +36,27 @@ class Model:
         #divide the self.data into test and training sets
         X = self.data.drop(['price'], axis='columns')
         y = self.data['price'].values
+        
+        # normalize non-dummy columns using MinMaxScaler
+        self.sc = MinMaxScaler()
+        X[['latitude','longitude','inflation','year','month','month2','longlat','inflyr','locyr','locmth']] = self.sc.fit_transform(X[['latitude','longitude','inflation','year','month','month2','longlat','inflyr','locyr','locmth']])
 
-        # add interactions for each commodity
+
+        # For each commodity, add a new interaction feature with each of the non-commodity predictors in the dataset
         for i in X.columns:
-            if i not in ['locyr','locmth','inflation','year','inflyr','longlat','latitude','longitude','month','month2','priceflag_actual','priceflag_actual,aggregate','priceflag_aggregate','pricetype_Farm Gate','pricetype_Retail','pricetype_Wholesale']:
-                for j in ['locyr','locmth','inflation','year','inflyr','longlat','latitude','longitude','month','month2','priceflag_actual','priceflag_actual,aggregate','priceflag_aggregate','pricetype_Farm Gate','pricetype_Retail','pricetype_Wholesale']:
-                     X.insert(len(X.columns),i+j,X[i]*X[j])
+            if i not in ['inflation','year','inflyr','longlat','latitude','longitude','locyr','locmth','month','month2','location','priceflag_actual','priceflag_actual,aggregate','priceflag_aggregate','pricetype_Farm Gate','pricetype_Retail','pricetype_Wholesale']:
+                for j in ['year','inflyr','locyr','locmth','latitude','longitude','longlat','month','month2','priceflag_actual','priceflag_actual,aggregate','priceflag_aggregate','pricetype_Farm Gate','pricetype_Retail','pricetype_Wholesale']:
+                    X.insert(len(X.columns),i+' x '+j,X[i]*X[j])
                     #X=pd.concat((X,(X[i]*X[j]).rename(i+j)),axis=1)
+
+        # remove commodity-dependent presictors
+        X = X.drop(['month','month2','locmth','priceflag_actual','priceflag_actual,aggregate','priceflag_aggregate','pricetype_Farm Gate','pricetype_Retail','pricetype_Wholesale'], axis='columns')
                     
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=26)
 
         #Train the model using the training set
 
-        self.regressor = LinearRegression()
+        self.regressor = Ridge(alpha=0.01)
         self.regressor.fit(X_train, y_train)
     
     def getPrice(self, params):
@@ -61,8 +70,8 @@ class Model:
                       'commodity_Mangoes (carabao)':params.setdefault('mango',0), 'commodity_Rice (regular, milled)':params.setdefault('rice (regular)',0),
                       'commodity_Rice (special)':params.setdefault('rice (special)',0), 'commodity_Eggs':params.setdefault('eggs',0),
                       'commodity_Bananas (lakatan)':params.setdefault('banana',0),'commodity_Meat (chicken, whole)':params.setdefault('chicken',0),
-                      'priceflag_actual':1, 'pricetype_Retail':params.setdefault('Retail',0),
-                      'pricetype_Wholesale':params.setdefault('Wholesale',0)}, ignore_index=True)
+                      'priceflag_actual':1, 'pricetype_Retail':params.setdefault('retail',0),
+                      'pricetype_Wholesale':params.setdefault('wholesale',0)}, ignore_index=True)
         
         ### PREPROCESS TO MATCH DF FORMAT
 
@@ -73,10 +82,18 @@ class Model:
         d['locyr'] = d['inflyr']*d['longlat'] # location-year relationship
         d['locmth'] = d['month']*d['month2']*d['longlat'] # location-month relationship
 
-        for i in d.columns:
-            if i not in ['locyr','locmth','inflation','year','inflyr','longlat','latitude','longitude','month','month2','priceflag_actual','priceflag_actual,aggregate','priceflag_aggregate','pricetype_Farm Gate','pricetype_Retail','pricetype_Wholesale']:
-                for j in ['locyr','locmth','inflation','year','inflyr','longlat','latitude','longitude','month','month2','priceflag_actual','priceflag_actual,aggregate','priceflag_aggregate','pricetype_Farm Gate','pricetype_Retail','pricetype_Wholesale']:
-                    #d=pd.concat((d,(d[i]*d[j]).rename(i+j)),axis=1)
-                    d.insert(len(d.columns),i+j,d[i]*d[j])
+        d[['latitude','longitude','inflation','year','month','month2','longlat','inflyr','locyr','locmth']] = self.sc.transform(d[['latitude','longitude','inflation','year','month','month2','longlat','inflyr','locyr','locmth']])
 
-        return self.regressor.predict(d)
+        # For each commodity, add a new interaction feature with each of the non-commodity predictors in the dataset
+        for i in d.columns:
+            if i not in ['inflation','year','inflyr','longlat','latitude','longitude','locyr','locmth','month','month2','location','priceflag_actual','priceflag_actual,aggregate','priceflag_aggregate','pricetype_Farm Gate','pricetype_Retail','pricetype_Wholesale']:
+                for j in ['year','inflyr','locyr','locmth','latitude','longitude','longlat','month','month2','priceflag_actual','priceflag_actual,aggregate','priceflag_aggregate','pricetype_Farm Gate','pricetype_Retail','pricetype_Wholesale']:
+                    d.insert(len(d.columns),i+' x '+j,d[i]*d[j])
+                    #X=pd.concat((X,(X[i]*X[j]).rename(i+j)),axis=1)
+
+        # remove commodity-dependent presictors
+        d = d.drop(['month','month2','locmth','priceflag_actual','priceflag_actual,aggregate','priceflag_aggregate','pricetype_Farm Gate','pricetype_Retail','pricetype_Wholesale'], axis='columns')
+
+        print(params)
+        params.update({'price': self.regressor.predict(d)[0]})
+        return params
